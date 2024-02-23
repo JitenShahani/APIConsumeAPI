1ec3da18f69517faa8cc35fd19e42a1e2df4f297
Jiten Shahani <shahani.jiten@gmail.com>
2024-02-23 19:27:41 +0530

    Added Reverse string endpoint to basic endpoints class

--- APIConsumeAPI/Endpoints/BasicEndpoints.cs
+++ APIConsumeAPI/Endpoints/BasicEndpoints.cs
@@ -10,19 +10,30 @@ public class BasicEndpoints
 			.DisableRateLimiting()
 			.WithOpenApi();
 
-		endpoint
+		group
 			.MapGet("hello", () => $"Hello, World! {DateTime.Now.ToLongDateString()}")
+			.WithTags("Basic Endpoints")
+			.WithGroupName("v1")
 			.Produces<string>(200)
 			.WithMetadata(new SwaggerOperationAttribute("Hello, World! - Current Date as long string"));
 
-		endpoint
+		group
 			.MapPost("/user", () =>
 			{
 				IEnumerable<string> users = [ "James", "Jon", "Steve", "Mads" ];
 
 				return TypedResults.Ok(users);
 			})
+			.WithTags("Basic Endpoints")
+			.WithGroupName("v1")
 			.Produces<IEnumerable<string>>(200)
 			.WithMetadata(new SwaggerOperationAttribute("List of users"));
+
+		group
+			.MapGet("/reverseString", (string input) => new string(input.Reverse().ToArray()))
+			.WithTags("Basic Endpoints")
+			.WithGroupName("v1")
+			.Produces<string>(200)
+			.WithMetadata(new SwaggerOperationAttribute("Reverse a string"));
 	}
 }
